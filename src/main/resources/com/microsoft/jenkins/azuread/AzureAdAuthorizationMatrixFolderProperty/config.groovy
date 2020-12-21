2850b4768d041fecdc91411ada20b3053a97889b
Tim Jacomb <timjacomb1+github@gmail.com>
2020-12-21 11:49:51 +0000

    JENKINS-64452 Compatibility with Jenkins core >=2.271

--- src/main/resources/com/microsoft/jenkins/azuread/AzureAdAuthorizationMatrixFolderProperty/config.groovy
+++ src/main/resources/com/microsoft/jenkins/azuread/AzureAdAuthorizationMatrixFolderProperty/config.groovy
@@ -3,15 +3,13 @@ package com.microsoft.jenkins.azuread.AzureAdAuthorizationMatrixFolderProperty
 import lib.FormTagLib
 import org.jenkinsci.plugins.matrixauth.inheritance.InheritanceStrategyDescriptor
 
-import lib.FormTagLib
-import org.jenkinsci.plugins.matrixauth.inheritance.InheritanceStrategyDescriptor
-
 def f = namespace(FormTagLib)
+def a = namespace("/lib/azuread")
 def st = namespace("jelly:stapler")
 
 f.optionalBlock(name: 'useProjectSecurity', checked: instance != null, title: _("Enable project-based security")) {
     f.nested {
-        table(style: "width: 100%") {
+        a.blockWrapper {
             f.dropdownDescriptorSelector(title: _("Inheritance Strategy"), descriptors: InheritanceStrategyDescriptor.getApplicableDescriptors(my.class), field: 'inheritanceStrategy')
             st.include(class: "com.microsoft.jenkins.azuread.AzureAdMatrixAuthorizationStrategy", page: "config")
         }
