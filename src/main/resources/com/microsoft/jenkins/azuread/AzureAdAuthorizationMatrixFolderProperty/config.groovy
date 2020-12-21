b7cb1094d36f9b4b7cbe394f8d7b15b482b409ed
Tim Jacomb <timjacomb1+github@gmail.com>
2020-12-21 14:38:57 +0000

    No need for compatibility as core is high

--- src/main/resources/com/microsoft/jenkins/azuread/AzureAdAuthorizationMatrixFolderProperty/config.groovy
+++ src/main/resources/com/microsoft/jenkins/azuread/AzureAdAuthorizationMatrixFolderProperty/config.groovy
@@ -9,7 +9,7 @@ def st = namespace("jelly:stapler")
 
 f.optionalBlock(name: 'useProjectSecurity', checked: instance != null, title: _("Enable project-based security")) {
     f.nested {
-        a.blockWrapper {
+        div {
             f.dropdownDescriptorSelector(title: _("Inheritance Strategy"), descriptors: InheritanceStrategyDescriptor.getApplicableDescriptors(my.class), field: 'inheritanceStrategy')
             st.include(class: "com.microsoft.jenkins.azuread.AzureAdMatrixAuthorizationStrategy", page: "config")
         }
