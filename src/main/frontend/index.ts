7b29ca541989553a530d35b49921eb07381aac1b
Tim Jacomb <21194782+timja@users.noreply.github.com>
2023-12-19 15:34:22 +0000

    Add theme manager integration (#524)

--- src/main/frontend/index.ts
+++ src/main/frontend/index.ts
@@ -1,7 +1,31 @@
 import { Providers } from '@microsoft/mgt-element/dist/es6';
 import { ProxyProvider } from '@microsoft/mgt-proxy-provider/dist/es6/ProxyProvider';
 import '@microsoft/mgt-components';
+import { applyTheme } from "@microsoft/mgt-components";
 
+document.addEventListener('DOMContentLoaded', () => {
+    const peoplePicker = document.querySelector(".entra-id-people-picker");
+
+    const anyWindow = window as any;
+
+    function getTheme() {
+        return anyWindow.getThemeManagerProperty('entra-id', 'theme')
+    }
+
+    if (anyWindow.getThemeManagerProperty) {
+        const peopleManagerTheme = getTheme();
+        if (peopleManagerTheme) {
+            const setTheme = () => applyTheme(getTheme(), peoplePicker as HTMLElement);
+            setTheme();
+
+            if (anyWindow.isSystemRespectingTheme) {
+                window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+                    setTheme()
+                });
+            }
+        }
+    }
+})
 const currentUrl = window.location.href
 
 // GraphProxy is either a root action or at the job level
