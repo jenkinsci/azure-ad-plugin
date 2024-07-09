4362a23d85766626205b9a63d83d98a330f2d847
Itzik Bekel <itzikbekel@microsoft.com>
2024-07-09 17:28:59 +0300

    Initial

--- src/main/java/com/microsoft/jenkins/azuread/AzureSecurityRealm.java
+++ src/main/java/com/microsoft/jenkins/azuread/AzureSecurityRealm.java
@@ -335,12 +335,12 @@
      }
  
      @DataBoundConstructor
-     public AzureSecurityRealm(String tenant, String clientId, Secret clientSecret, Secret pemCertificate, boolean isEnableClientCertificate, int cacheDuration) {
+     public AzureSecurityRealm(String tenant, String clientId, Secret clientSecret, Secret pemCertificate, boolean enableClientCertificate, int cacheDuration) {
          super();
          this.clientId = Secret.fromString(clientId);
          this.clientSecret = clientSecret;
          this.pemCertificate = pemCertificate;
-         this.isEnableClientCertificate = isEnableClientCertificate();
+         this.enableClientCertificate = enableClientCertificate;
          this.tenant = Secret.fromString(tenant);
          this.cacheDuration = cacheDuration;
          caches = Caffeine.newBuilder()
