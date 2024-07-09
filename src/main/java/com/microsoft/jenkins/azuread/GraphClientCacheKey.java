44bd59935d3763e7e6dfe551504e931313ecc3b2
Itzik Bekel <itzikbekel@microsoft.com>
2024-07-09 15:57:49 +0300

    Initial

--- src/main/java/com/microsoft/jenkins/azuread/GraphClientCacheKey.java
+++ src/main/java/com/microsoft/jenkins/azuread/GraphClientCacheKey.java
@@ -3,30 +3,39 @@ package com.microsoft.jenkins.azuread;
 import java.util.Objects;
 
 class GraphClientCacheKey {
-        private final String clientId;
-        private final String clientSecret;
-        private final String tenantId;
-        private final String azureEnvironmentName;
+    private final String clientId;
+    private final String clientSecret;
+    private final String pemCertificate;
+    private final String tenantId;
+    private final String azureEnvironmentName;
+    private final boolean isEnableClientCertificate;
 
-        public GraphClientCacheKey(String clientId, String clientSecret, String tenantId, String azureEnvironmentName) {
-            this.clientId = clientId;
-            this.clientSecret = clientSecret;
-            this.tenantId = tenantId;
-            this.azureEnvironmentName = azureEnvironmentName;
-        }
+    public GraphClientCacheKey(String clientId, String clientSecret, String pemCertificate, String tenantId, String azureEnvironmentName, boolean isEnableClientCertificate) {
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.pemCertificate = pemCertificate;
+        this.tenantId = tenantId;
+        this.azureEnvironmentName = azureEnvironmentName;
+        this.isEnableClientCertificate = isEnableClientCertificate;
+    }
 
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            GraphClientCacheKey cacheKey = (GraphClientCacheKey) o;
-            return Objects.equals(clientId, cacheKey.clientId) && Objects.equals(clientSecret, cacheKey.clientSecret) && Objects.equals(tenantId, cacheKey.tenantId) && Objects.equals(azureEnvironmentName, cacheKey.azureEnvironmentName);
-        }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GraphClientCacheKey cacheKey = (GraphClientCacheKey) o;
+        return isEnableClientCertificate == cacheKey.isEnableClientCertificate &&
+                Objects.equals(clientId, cacheKey.clientId) &&
+                Objects.equals(clientSecret, cacheKey.clientSecret) &&
+                Objects.equals(pemCertificate, cacheKey.pemCertificate) &&
+                Objects.equals(tenantId, cacheKey.tenantId) &&
+                Objects.equals(azureEnvironmentName, cacheKey.azureEnvironmentName);
+    }
 
-        @Override
-        public int hashCode() {
-            return Objects.hash(clientId, clientSecret, tenantId, azureEnvironmentName);
-        }
+    @Override
+    public int hashCode() {
+        return Objects.hash(clientId, clientSecret, pemCertificate, tenantId, azureEnvironmentName, isEnableClientCertificate);
+    }
 
     public String getClientId() {
         return clientId;
@@ -36,11 +45,19 @@ class GraphClientCacheKey {
         return clientSecret;
     }
 
+    public String getPemCertificate() {
+        return pemCertificate;
+    }
+
     public String getTenantId() {
         return tenantId;
     }
 
     public String getAzureEnvironmentName() {
         return azureEnvironmentName;
+    }
+
+    public boolean isEnableClientCertificate() {
+        return isEnableClientCertificate;
     }
 }
