5d2aa9d02c8aba3ee6b4862bd027875c4565771e
Itzik Bekel <itzikbekel@microsoft.com>
2024-07-15 18:55:56 +0300

    adjust graph client class

--- src/main/java/com/microsoft/jenkins/azuread/GraphClientCacheKey.java
+++ src/main/java/com/microsoft/jenkins/azuread/GraphClientCacheKey.java
@@ -3,55 +3,55 @@ package com.microsoft.jenkins.azuread;
 import java.util.Objects;
 
 class GraphClientCacheKey {
-    private final String clientId;
-    private final String clientSecret;
-    private final String clientCertificate;
-    private final String tenantId;
-    private final String azureEnvironmentName;
-    private final String credentialType;
+        private final String clientId;
+        private final String clientSecret;
+        private final String clientCertificate;
+        private final String tenantId;
+        private final String azureEnvironmentName;
+        private final String credentialType;
 
-    public GraphClientCacheKey(String clientId, String clientSecret, String clientCertificate,String credentialType, String tenantId, String azureEnvironmentName) {
-        this.clientId = clientId;
-        this.clientSecret = clientSecret;
-        this.clientCertificate = clientCertificate;
-        this.credentialType = credentialType;
-        this.tenantId = tenantId;
-        this.azureEnvironmentName = azureEnvironmentName;
-    }
+        public GraphClientCacheKey(String clientId, String clientSecret, String clientCertificate,String credentialType, String tenantId, String azureEnvironmentName) {
+            this.clientId = clientId;
+            this.clientSecret = clientSecret;
+            this.clientCertificate = clientCertificate;
+            this.credentialType = credentialType;
+            this.tenantId = tenantId;
+            this.azureEnvironmentName = azureEnvironmentName;
+        }
 
-    @Override
-        public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        GraphClientCacheKey that = (GraphClientCacheKey) o;
-        return  Objects.equals(clientId, that.clientId) &&
-                Objects.equals(clientSecret, that.clientSecret) &&
-                Objects.equals(clientCertificate, that.clientCertificate) &&
-                Objects.equals(credentialType, that.credentialType) &&
-                Objects.equals(tenantId, that.tenantId) &&
-                Objects.equals(azureEnvironmentName, that.azureEnvironmentName);
-    }
+        @Override
+            public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            GraphClientCacheKey that = (GraphClientCacheKey) o;
+            return  Objects.equals(clientId, that.clientId) &&
+                    Objects.equals(clientSecret, that.clientSecret) &&
+                    Objects.equals(clientCertificate, that.clientCertificate) &&
+                    Objects.equals(credentialType, that.credentialType) &&
+                    Objects.equals(tenantId, that.tenantId) &&
+                    Objects.equals(azureEnvironmentName, that.azureEnvironmentName);
+        }
 
-    @Override
-        public int hashCode() {
-        return Objects.hash(clientId, clientSecret, clientCertificate, credentialType, tenantId, azureEnvironmentName);
-    }
-    public String getClientId() {
-        return clientId;
-    }
-    public String getClientSecret() {
-        return clientSecret;
-    }
-    public String getClientCertificate() {
-        return clientCertificate;
-    }
-    public String getCredentialType() {
-        return credentialType;
-    }
-    public String getTenantId() {
-        return tenantId;
-    }
-    public String getAzureEnvironmentName() {
-        return azureEnvironmentName;
-    }
+        @Override
+            public int hashCode() {
+            return Objects.hash(clientId, clientSecret, clientCertificate, credentialType, tenantId, azureEnvironmentName);
+        }
+        public String getClientId() {
+            return clientId;
+        }
+        public String getClientSecret() {
+            return clientSecret;
+        }
+        public String getClientCertificate() {
+            return clientCertificate;
+        }
+        public String getCredentialType() {
+            return credentialType;
+        }
+        public String getTenantId() {
+            return tenantId;
+        }
+        public String getAzureEnvironmentName() {
+            return azureEnvironmentName;
+        }
 }
