f4054844eae7e96e0c2739403ea26bd0c9341ec6
Markus Gaisbauer <markus.gaisbauer@dynatrace.com>
2024-07-19 14:33:48 +0200

    Add test + benchmark for ObjId2FullSidMap::extractObjectId

--- src/test/java/com/microsoft/jenkins/azuread/ObjId2FullSidMapTest.java
+++ src/test/java/com/microsoft/jenkins/azuread/ObjId2FullSidMapTest.java
@@ -1,7 +1,11 @@
 package com.microsoft.jenkins.azuread;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ObjId2FullSidMapTest {
 
@@ -29,4 +33,49 @@ public class ObjId2FullSidMapTest {
         Assert.assertEquals(FULL_SID_1, map.getOrOriginal(ObjId2FullSidMap.generateFullSid(NAME_1, OBJECT_ID_1)));
         Assert.assertEquals("some string", map.getOrOriginal("some string"));
     }
+
+    @Test
+    public void testExtractObjectId() {
+        Assert.assertNull(ObjId2FullSidMap.extractObjectId(""));
+        Assert.assertNull(ObjId2FullSidMap.extractObjectId("some string"));
+        Assert.assertNull(ObjId2FullSidMap.extractObjectId("email (id) "));
+        Assert.assertNull(ObjId2FullSidMap.extractObjectId("email (id"));
+        Assert.assertNull(ObjId2FullSidMap.extractObjectId("email id)"));
+        Assert.assertNull(ObjId2FullSidMap.extractObjectId("email(id)"));
+        Assert.assertEquals("id", ObjId2FullSidMap.extractObjectId("email (id)"));
+        Assert.assertEquals("", ObjId2FullSidMap.extractObjectId("email ()"));
+        Assert.assertEquals("id", ObjId2FullSidMap.extractObjectId(" (id)"));
+    }
+
+    @Ignore
+    @Test
+    public void testExtractObjectIdPerformance() throws Exception {
+        final int numWarmupIterations = 1_000_000;
+        final int numExperiments = 1000;
+        final int numIterations = 10000;
+
+        final String fullSid = ObjId2FullSidMap.generateFullSid(EMAIL_1, OBJECT_ID_1);
+
+        // warmup
+        for (int i = 0; i < numWarmupIterations; i++) {
+            ObjId2FullSidMap.extractObjectId(fullSid);
+        }
+        // allow async JIT compilation to catch up
+        Thread.sleep(100);
+
+        List<Long> durationsNanos = new ArrayList<>();
+        for (int r = 0; r < numExperiments; r++) {
+            long start = System.nanoTime();
+            for (int i = 0; i < numIterations; i++) {
+                ObjId2FullSidMap.extractObjectId(fullSid);
+            }
+            long durationNanos = (System.nanoTime() - start) / numIterations;
+            durationsNanos.add(durationNanos);
+        }
+        durationsNanos.sort(null);
+        long median = durationsNanos.get(durationsNanos.size() / 2);
+
+        System.out.println("Median duration per call (nanos): " + median);
+    }
+
 }
