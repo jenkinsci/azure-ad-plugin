5eed7ac68693138700133874697d8b927bbfa94c
Tim Jacomb <timjacomb1@gmail.com>
2025-02-14 16:15:46 +0000

    Revert "Migrate tests to JUnit5 (#662)"
    This reverts commit 2831d05abf41270eb8fddbe4d9f567a291ba1423.

--- src/test/java/com/microsoft/jenkins/azuread/integrations/casc/BaseConfigAsCodeTest.java
+++ src/test/java/com/microsoft/jenkins/azuread/integrations/casc/BaseConfigAsCodeTest.java
@@ -10,11 +10,10 @@ import hudson.model.Item;
 import hudson.model.Node;
 import hudson.security.AuthorizationStrategy;
 import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
-import io.jenkins.plugins.casc.misc.junit.jupiter.WithJenkinsConfiguredWithCode;
 import jenkins.model.Jenkins;
 import org.jenkinsci.plugins.matrixauth.inheritance.NonInheritingStrategy;
-import org.jvnet.hudson.test.LogRecorder;
-
+import org.junit.Rule;
+import org.jvnet.hudson.test.LoggerRule;
 import java.util.logging.Level;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -22,57 +21,59 @@ import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
-@WithJenkinsConfiguredWithCode
-abstract class BaseConfigAsCodeTest {
+public abstract class BaseConfigAsCodeTest {
     protected static final String TEST_UPN = "abc@jenkins.com";
-
-    protected final LogRecorder logRecorder = new LogRecorder().record(AzureAdMatrixAuthorizationStrategy.class, Level.WARNING).capture(20);
-
+    
+    @Rule
+    public LoggerRule l = new LoggerRule().record(AzureAdMatrixAuthorizationStrategy.class, Level.WARNING).capture(20);
     protected void validateCommonAssertions(JenkinsConfiguredWithCodeRule j) {
         AuthorizationStrategy authorizationStrategy = j.jenkins.getAuthorizationStrategy();
-        assertInstanceOf(AzureAdMatrixAuthorizationStrategy.class, authorizationStrategy, "authorization strategy");
+        assertTrue("authorization strategy", authorizationStrategy instanceof AzureAdMatrixAuthorizationStrategy);
         AzureAdMatrixAuthorizationStrategy azureAdMatrixAuthorizationStrategy = (AzureAdMatrixAuthorizationStrategy) authorizationStrategy;
 
-        assertEquals(2, azureAdMatrixAuthorizationStrategy.getAllPermissionEntries().size(), "one real user sid");
-        assertTrue(azureAdMatrixAuthorizationStrategy.hasExplicitPermission(PermissionEntry.user("anonymous"), Jenkins.READ), "anon can read");
-        assertTrue(azureAdMatrixAuthorizationStrategy.hasExplicitPermission(PermissionEntry.user(TEST_UPN), Jenkins.READ), "authenticated can read");
-        assertTrue(azureAdMatrixAuthorizationStrategy.hasExplicitPermission(PermissionEntry.group("authenticated"), Item.BUILD), "authenticated can build");
-        assertTrue(azureAdMatrixAuthorizationStrategy.hasExplicitPermission(PermissionEntry.user(TEST_UPN), Item.DELETE), "authenticated can delete jobs");
-        assertTrue(azureAdMatrixAuthorizationStrategy.hasExplicitPermission(PermissionEntry.user(TEST_UPN), Jenkins.ADMINISTER), "authenticated can administer");
-        assertTrue(logRecorder.getMessages().isEmpty(), "no warnings");
+        assertEquals("one real user sid", 2, azureAdMatrixAuthorizationStrategy.getAllPermissionEntries().size());
+        assertTrue("anon can read", azureAdMatrixAuthorizationStrategy.hasExplicitPermission(PermissionEntry.user("anonymous"), Jenkins.READ));
+        assertTrue("authenticated can read", azureAdMatrixAuthorizationStrategy.hasExplicitPermission(PermissionEntry.user(TEST_UPN), Jenkins.READ));
+        assertTrue("authenticated can build", azureAdMatrixAuthorizationStrategy.hasExplicitPermission(PermissionEntry.group("authenticated"), Item.BUILD));
+        assertTrue("authenticated can delete jobs", azureAdMatrixAuthorizationStrategy.hasExplicitPermission(PermissionEntry.user(TEST_UPN), Item.DELETE));
+        assertTrue("authenticated can administer", azureAdMatrixAuthorizationStrategy.hasExplicitPermission(PermissionEntry.user(TEST_UPN), Jenkins.ADMINISTER));
+        assertEquals("no warnings", 0, l.getMessages().size());
 
-        Node agent = j.jenkins.getNode("agent");
-        assertThat(agent, is(notNullValue()));
-        assertThat(agent.getDisplayName(), is(equalTo("agent")));
-        AzureAdAuthorizationMatrixNodeProperty nodeProperty =
-                agent.getNodeProperty(AzureAdAuthorizationMatrixNodeProperty.class);
-        assertThat(nodeProperty, is(notNullValue()));
-        assertThat(nodeProperty.getInheritanceStrategy(), instanceOf(NonInheritingStrategy.class));
-        assertThat(
-                nodeProperty
-                        .hasExplicitPermission(PermissionEntry.user("Adele Vance (be674052-e519-4231-b5e7-2b390bff6346)"),
-                                Computer.BUILD),
-                is(true)
-        );
+        {
+            Node agent = j.jenkins.getNode("agent");
+            assertThat(agent, is(notNullValue()));
+            assertThat(agent.getDisplayName(), is(equalTo("agent")));
+            AzureAdAuthorizationMatrixNodeProperty nodeProperty =
+                    agent.getNodeProperty(AzureAdAuthorizationMatrixNodeProperty.class);
+            assertThat(nodeProperty, is(notNullValue()));
+            assertThat(nodeProperty.getInheritanceStrategy(), instanceOf(NonInheritingStrategy.class));
+            assertThat(
+                    nodeProperty
+                            .hasExplicitPermission(PermissionEntry.user("Adele Vance (be674052-e519-4231-b5e7-2b390bff6346)"),
+                                    Computer.BUILD),
+                    is(true)
+            );
+        }
 
-        Folder folder = (Folder) j.jenkins.getItem("generated");
-        assertNotNull(folder);
-        AzureAdAuthorizationMatrixFolderProperty property = folder.getProperties()
-                .get(AzureAdAuthorizationMatrixFolderProperty.class);
-        assertInstanceOf(NonInheritingStrategy.class, property.getInheritanceStrategy(), "folder property inherits");
-        String groupSid = "Some group (7fe913e8-6c9f-40f8-913e-7178b7768cc5)";
-        assertTrue(property.hasExplicitPermission(PermissionEntry.group(groupSid), Item.BUILD));
-        assertTrue(property.hasExplicitPermission(PermissionEntry.group(groupSid), Item.READ));
-        assertFalse(property.hasExplicitPermission(PermissionEntry.user("anonymous"), Item.READ));
-        assertTrue(property.hasExplicitPermission(PermissionEntry.group(groupSid), Item.CONFIGURE));
-        assertTrue(property.hasExplicitPermission(PermissionEntry.group(groupSid), Item.DELETE));
-        String userSid = "c411116f-cfa6-472c-8ccf-d0cb6053c9aa";
-        assertTrue(property.hasExplicitPermission(PermissionEntry.user(userSid), Item.BUILD));
+        {
+            Folder folder = (Folder) j.jenkins.getItem("generated");
+            assertNotNull(folder);
+            AzureAdAuthorizationMatrixFolderProperty property = folder.getProperties()
+                    .get(AzureAdAuthorizationMatrixFolderProperty.class);
+            assertTrue("folder property inherits", property.getInheritanceStrategy() instanceof NonInheritingStrategy);
+            String groupSid = "Some group (7fe913e8-6c9f-40f8-913e-7178b7768cc5)";
+            assertTrue(property.hasExplicitPermission(PermissionEntry.group(groupSid), Item.BUILD));
+            assertTrue(property.hasExplicitPermission(PermissionEntry.group(groupSid), Item.READ));
+            assertFalse(property.hasExplicitPermission(PermissionEntry.user("anonymous"), Item.READ));
+            assertTrue(property.hasExplicitPermission(PermissionEntry.group(groupSid), Item.CONFIGURE));
+            assertTrue(property.hasExplicitPermission(PermissionEntry.group(groupSid), Item.DELETE));
+            String userSid = "c411116f-cfa6-472c-8ccf-d0cb6053c9aa";
+            assertTrue(property.hasExplicitPermission(PermissionEntry.user(userSid), Item.BUILD));
+        }
     }
 }
--- src/test/java/com/microsoft/jenkins/azuread/utils/UUIDValidatorTest.java
+++ src/test/java/com/microsoft/jenkins/azuread/utils/UUIDValidatorTest.java
@@ -1,19 +1,18 @@
 package com.microsoft.jenkins.azuread.utils;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-class UUIDValidatorTest {
+public class UUIDValidatorTest {
 
     @Test
-    void is_valid_uuid_true() {
+    public void is_valid_uuid_true(){
         assertTrue(UUIDValidator.isValidUUID("009692ee-f930-4a74-bbd0-63b8baa5a927"));
     }
-
     @Test
-    void is_valid_uuid_false() {
+    public void is_valid_uuid_false(){
         assertFalse(UUIDValidator.isValidUUID(null));
         assertFalse(UUIDValidator.isValidUUID(""));
         assertFalse(UUIDValidator.isValidUUID("test-ss-ss-ss-s"));
